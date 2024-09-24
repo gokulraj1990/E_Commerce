dc81647f9bd3ffe4364ea670d54ecfc56021421e
Navaneethan-cnk <krishnancse18@gmail.com>
2024-09-24 14:30:27 +0530

    Update totalPrice calculation in Order model

--- Order_Management/serializers.py
+++ Order_Management/serializers.py
@@ -4,7 +4,8 @@ from .models import Order
 class OrderSerializer(serializers.ModelSerializer):
     class Meta:
         model = Order
-        fields = '__all__'
+        fields = ['orderID', 'customerID', 'productID', 'quantity', 'totalPrice', 'orderDate', 'status']
+        read_only_fields = ['orderID', 'totalPrice', 'orderDate']
 
     def validate(self, data):
         if data['quantity'] <= 0:
